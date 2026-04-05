707a22390914495d807f3e7ec6d0190eaa4d1476
Sumant J <145242132+Sumant40@users.noreply.github.com>
2026-04-05 16:31:46 +0530

    Add URL  for African cities problem SQL query

--- Africa_cities_name.sql
+++ Africa_cities_name.sql
@@ -1,3 +1,4 @@
+--https://hackerrank-challenge-pdfs.s3.amazonaws.com/8343-african-cities-English?response-content-disposition=inline%3B%20filename%3Dafrican-cities-English.pdf&response-content-type=application%2Fpdf&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAR6O7GJNX2PJLRWX4%2F20260405%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20260405T110043Z&X-Amz-Expires=3600&X-Amz-SignedHeaders=host&X-Amz-Signature=95925f93213c8d8703cc8ce5ca39e53448e740b6b46cdb672f26fee914555716
 SELECT ci.name as city_names
 FROM CITY as ci 
 JOIN COUNTRY as co
